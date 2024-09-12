d057864d3bfb05875ea86bb45f14d86fe208a1df
John Major <daylily@daylilyinformatics.com>
2024-09-11 22:46:56 -0700

    v0.2.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mirrormirror",
-    version="0.2.4",
+    version="0.2.5",
     author="John Major",  # Replace with your name
     author_email="iamh2o+mirrormirror@gmail.com",  # Replace with your email
     packages=find_packages(),
